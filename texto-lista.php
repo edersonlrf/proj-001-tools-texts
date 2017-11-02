c7ece691aa3e76f73a6d3d65e37c553c373f4670
edersonlrf <edersonluis.rf@gmail.com>
2017-11-02 21:02:16 -0200

    save

--- texto-lista.php
+++ texto-lista.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once("cabecalho.php");
+require_once "cabecalho.php";
 
-require_once("logica-usuario.php");
+require_once "logica-usuario.php";
 
 $textoDAO = new TextoDAO($conexao);
 
@@ -22,17 +22,18 @@ $textos = $textoDAO->listaTextos();
             <!-- <th>ISBN</th> -->
             <th>Ação 1</th>
             <th>Ação 2</th>
+            <th>Ação 3</th>
         </tr>
     </thead>
     <tbody>
     <?php
-    foreach($textos as $texto) :
-    ?>
+foreach ($textos as $texto):
+?>
         <tr>
-            <td><?= $texto->getNumero() ?></td>
-            <td><?= $texto->getTitulo() ?></td>
-            <td><?= $texto->getIngles() ?></td>
-            <td><?= $texto->getPortugues(); ?></td>
+            <td><?php echo $texto->getNumero();?></td>
+            <td><?php echo $texto->getTitulo();?></td>
+            <td><?php echo substr($texto->getIngles(), 0, 150) . '...';?></td>
+            <td><?php echo substr($texto->getPortugues(), 0, 150) . '...';?></td>
             <!-- <td>< ?= substr($texto->getDescricao(), 0, 40) ?></td> -->
             <!-- <td>< ?= $texto->getCategoria()->getNome() ?></td> -->
             <!-- <td>
@@ -42,20 +43,27 @@ $textos = $textoDAO->listaTextos();
                     }
                 ?>
             </td> -->
-            <td><a class="btn btn-primary" href="texto-altera-formulario.php?id=<?=$texto->getId()?>">Alterar</a></td>
+            <td><a class="btn btn-primary" href="texto-altera-formulario.php?id=<?php echo $texto->getId();?>">Alterar</a></td>
             <td>
                 <!-- Não remover com link, pois o GET server somente para pegar dados, e tmb pq se o robo do Google acessar esta pagina, ele vai apagar tudo. -->
                 <!-- <a href="remove-texto.php?id=< ?= $texto['id'] ? >" class="text-danger">remover</a> -->
                 <form action="remove-texto.php" method="post">
-                    <input type="hidden" name="id" value="<?=$texto->getId()?>" />
+                    <input type="hidden" name="id" value="<?php echo $texto->getId();?>" />
                     <button class="btn btn-danger">Remover</button>
                 </form>
             </td>
+            <td>
+                <a class="btn btn-success" href="texto-word-a.php?id=<?php echo $texto->getId();?>" target="_blank">A</a>
+                <a class="btn btn-success" href="texto-word-b1.php?id=<?php echo $texto->getId();?>" target="_blank">B1</a>
+                <a class="btn btn-success" href="texto-word-b2.php?id=<?php echo $texto->getId();?>" target="_blank">B2</a>
+                <a class="btn btn-success" href="texto-word-c1.php?id=<?php echo $texto->getId();?>" target="_blank">C1</a>
+                <a class="btn btn-success" href="texto-word-c2.php?id=<?php echo $texto->getId();?>" target="_blank">C2</a>
+            </td>
         </tr>
     <?php
-    endforeach;
-    ?>
+endforeach;
+?>
     </tbody>
 </table>
 
-<?php require_once("rodape.php"); ?>
+<?php require_once "rodape.php";?>
