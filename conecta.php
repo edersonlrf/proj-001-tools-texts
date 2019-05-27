b9a077fee2091b377cf9277199d5735cc4674f26
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-05-26 23:53:00 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,3 +1,3 @@
 <?php
 
-$conexao = mysqli_connect("localhost", "root", "", "proj-001-tools-texts");
+$conexao = mysqli_connect("arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ma3cw9ezkos6ycr8", "mxjdrajmk6pp8s32", "afoj7l25bmjs95ra");
